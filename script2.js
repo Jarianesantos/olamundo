4f38d0a05e79a7d7192fdd5550c7a1a0ec1b8b9a
Jarianesantos <jrnjrn1234@gmail.com>
2022-10-06 19:20:37 -0300

    pojetoJARIANE

---
+++ script2.js
@@ -0,0 +1,27 @@
+
+
+var SOMA="1+2" 
+console.log(SOMA)
+
+//*
+
+let y=15
+let z=20
+console.log (y+z)
+
+//*
+
+var a=20
+var b=40
+console.log (a+b)
+
+//*
+const Name="ola Jariane"
+alert (Name)
+
+
+
+
+
+    
+    
